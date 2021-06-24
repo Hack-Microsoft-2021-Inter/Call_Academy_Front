9b5b42617a27a0902f30ff45ac6607d66d8cad6f
galvisgrjp <galvisgrjp@gmail.com>
2021-06-23 23:26:28 -0500

    Front adelantado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MonitorListComponent } from './monitor/monitor-list/monitor-list.component';
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: LandingPageComponent
+  },
+  {
+    path: "monitores",
+    component: MonitorListComponent
+  }
 ];
 
 @NgModule({
